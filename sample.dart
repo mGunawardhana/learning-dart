4e3bd7d409d1aada7cf3c3625e34f6cea339b79a
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-04-26 09:08:47 +0530

    content update

---
+++ sample.dart
@@ -0,0 +1,36 @@
+/*
+What is Flutter ?
+
+A "tool" or "framework" that allows you to build native
+cross-platform(ios, Android) apps with one programming language and
+codebase.
+
+it was firstly announced in 2015.
+
+google launched flutter 1.0, the stable version of flutter
+on 5th of December,2018
+
+we can separate it main two parts there are,
+
+1) framework/widget library
+    UI building blocks(widgets),utility functions,packages
+
+2) SDK
+    Tools to compile code to native machine code
+
+DART --
+
+  This is OOP language.
+  it was released on 2011.
+
+ */
+
+void main() {
+  int age = 23;
+
+  if (age > 18) {
+    print("you are adult!");
+  } else {
+    print("you are child !");
+  }
+}
