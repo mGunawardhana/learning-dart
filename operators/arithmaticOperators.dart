2fac4de96e093067f363024ac5053c3e7b2d6b1c
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-04-26 12:28:27 +0530

    arithmatic operators

--- operators/arithmaticOperators.dart
+++ operators/arithmaticOperators.dart
@@ -1,6 +1,8 @@
 void main() {
   var num1 = 10;
   var num2 = 20;
+  var num3 = 5;
+  var num4 = 2;
   var output = 0;
 
   /* Addition output - 30*/
@@ -16,4 +18,23 @@ void main() {
   /* Division output - 1.5 */
   /** because output value is 30 see code line 8 to understand, (num2 = 20) */
   print("output is: ${output / num2}");
+
+  /* Reminder output - 1 */
+  print("output is: ${num3 % num4}");
+  /** this symbol can return non dividing balance */
+
+  /* Division returning integer value - 2 */
+  print("output is: ${num3 ~/ num4}");
+  /** we can remove doted points value */
+
+  /* Incrementer -  3*/
+  print("before the increment: ${num4}");// 2
+  num4++;
+  print("after the increment: ${num4}");// 3
+
+  /* Decrement - 4 */
+  print("before the decrement: ${num3}");// 5
+  num3--;
+  print("after the decrement: ${num3}");// 3
+
 }
