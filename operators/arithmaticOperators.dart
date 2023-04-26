98b1ddc68cf23e1a057bb13731dadf6dcf31e1d4
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-04-26 10:11:17 +0530

    division

--- operators/arithmaticOperators.dart
+++ operators/arithmaticOperators.dart
@@ -3,8 +3,17 @@ void main() {
   var num2 = 20;
   var output = 0;
 
-  /* Addition */
+  /* Addition output - 30*/
   output = num1 + num2;
-  print("output is: ${num1 + num2}");
-  print(output);
+  print("output is: ${output}");
+
+  /* Subtraction output - 10 */
+  print("output is: ${num2 - num1}");
+
+  /* Multiplication output - 200 */
+  print("output is: ${num1 * num2}");
+
+  /* Division output - 1.5 */
+  /** because output value is 30 see code line 8 to understand, (num2 = 20) */
+  print("output is: ${output / num2}");
 }
