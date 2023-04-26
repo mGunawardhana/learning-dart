c5b66cf35231a5c9eb246bd94fd696435e7916b7
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-04-26 15:21:47 +0530

    not equal operator

--- logical_operators/logical_operators.dart
+++ logical_operators/logical_operators.dart
@@ -9,7 +9,11 @@
 void main() {
   var a = 10;
   var b = 20;
+
+//------------------ AND OPERATOR ------------------
+
   var res = (a < b) && (b > 10);
+  print(res);
 
   /*
   * Resolving the code ...
@@ -28,5 +32,40 @@ void main() {
   *   And our output is TRUE;
   *
   * */
-  print(res);
+
+  //------------------ OR OPERATOR ------------------
+
+  var res1 = (a > b) || (b < 10);
+  print(res1);
+
+  /*
+  * Resolving the code 2 ...
+  *
+  * level 01 ->
+  *       var res1 = (10>20)||(20<10);
+  *       because 10 is greater than 20 - > false
+  *       another one is 20 is less than 10 - > false
+  *
+  * Level 02 ->
+  *       var res1 = (false)||(false);
+  *
+  * Level 03 ->
+  *       we can print res1 now, it's value is false because or operator check's
+  *   if the any side if true it will return true, and both side return false it will gave false.
+  *   And our output is FALSE;
+  * */
+
+  //------------------ NOT OPERATOR ------------------
+  var res2 =!(a==b);
+  print(res2);
+
+  /*
+  * Resolving the code 2 ...
+  *
+  * level 01 ->
+  *       var res2 =! (10==20);
+  *       var res2 =! (false);
+  *       it will return false. getting to the opposite it will return true
+  *       out output is TRUE;
+  * */
 }
